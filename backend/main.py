00e6482e0fc4d7c63242d81ea7993b3ffa47e8ca
Vrushabh <vrushabhgawas14@gmail.com>
2025-10-05 16:23:59 +0530

    Backend Updated and Testing Deployment Bugs

--- backend/main.py
+++ backend/main.py
@@ -106,6 +106,57 @@ def summarize_text(text: str) -> str:
     except Exception as e:
         return f"Error: {str(e)}"
 
+
+# Text Queries 
+@app.post("/get-summary-of-text")
+async def get_summary_of_text(data: Dict[str, str]):
+    resolved_queries = resolve_query(data["text"].strip()) # Summarize
+    formatted_text = format_text_with_line_breaks(resolved_queries.strip()).removesuffix("\n")
+    # print("Final: \n",formatted_text)
+
+    return {"text": formatted_text}
+
+def resolve_query(text: str) -> str:
+    try:
+        stream = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": "You are an AI assistant that solves user queries."},
+                {"role": "user", "content": f"Most likely user will ask to Summarize the following text or anything else like chatting so the output will be a text and also avoid using bullet but use points if needed but in the new line. This was just a prerequisite to you rest depends on user input:\n\n{text}"}
+            ],
+            model="gpt-oss-120b",
+            stream=True,
+            max_completion_tokens=5000,
+            temperature=0.7,
+            top_p=1,
+            reasoning_effort="medium"
+        )
+
+        resolved_text = ""
+        for chunk in stream:
+            resolved_text += chunk.choices[0].delta.content or ""
+        return resolved_text.strip()
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+def format_text_with_line_breaks(text):
+    
+    lines = text.split(".\n") # Split after full stop
+    formatted_text = []
+    final_formatted_text = ""
+    
+    # print("Lines: .\-n\n",lines,"\n\n")
+
+    for i in range(len(lines)): 
+        formatted_text.append(lines[i] + ("." if i != len(lines) -1 else "")) 
+    
+    for i in range(len(formatted_text)):
+        subText = formatted_text[i]
+        final_formatted_text += subText + "\n"
+
+    # print("Formatted: replace('\-n','')",formatted_text, "\n\n")
+    return final_formatted_text
+
 if __name__ == "__main__":
     uvicorn.run(app,host="0.0.0.0",port=8000)
 
