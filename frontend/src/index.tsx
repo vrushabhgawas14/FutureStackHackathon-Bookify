6382232e2ea22a7cd6704a911a883cc157d13335
Vrushabh <vrushabhgawas14@gmail.com>
2025-10-05 17:54:38 +0530

    Final Commit

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/authContext";
 
+// Root Directory
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
